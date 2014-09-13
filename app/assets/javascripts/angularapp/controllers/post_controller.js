bfffd5a0ff8169a24b45a32cd8751145d75070ab
nicknish <n.nishiguchi9@gmail.com>
2014-09-13 01:32:45 -0700

    begin implemeting flip effect to posts

--- app/assets/javascripts/angularapp/controllers/post_controller.js
+++ app/assets/javascripts/angularapp/controllers/post_controller.js
@@ -2,4 +2,16 @@ opinionationApp.controller('PostCtrl', ['$scope', 'Post', function($scope, Post)
   Post.query(function(json){
     $scope.posts = json;
   });
-}])
+
+  // Flip checks if a post is flipped,
+  // which an ng-class will respond by adding or 
+  // removing the 'flipped' class
+  $scope.flip = function() {
+		var flipped = $scope.isFlipped;
+  	if (flipped == false || flipped == null) {
+  		$scope.isFlipped = true;
+  	} else { 
+  		$scope.isFlipped = false; 
+  	}
+  }
+}]);
