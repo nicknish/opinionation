0450e4a6bcf88d63b8697cd3a5de309a3a00a11c
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-16 09:23:05 -0700

    fix ability to unfavorite a post when the page loads and the post had been previously favorited

--- app/assets/javascripts/angularapp/controllers/favorite_controller.js
+++ app/assets/javascripts/angularapp/controllers/favorite_controller.js
@@ -1,23 +1,26 @@
 opinionationApp.controller('FavCtrl', ['$scope', 'Favorite', function($scope, Favorite){
 
+  //called by updateFavorite function
   $scope.addFavorite = function(post){
+    //instantiate a new empty Favorite object
     $scope.newFavorite = new Favorite();
     $scope.newFavorite.post_id = post.id;
     $scope.newFavorite.$save(function(x, putResponseHeaders){
-      console.log(x.id);
       post.post_favorite = x;
     });
   }
 
-  //CURRENTLY ONLY WORKS IF STARTING WITH UNFAVORITED POST
-  $scope.favoriteUpdate = function(post){
+  //called by ng-click in the posts index view
+  $scope.updateFavorite = function(post){
     if(post.post_favorite.id == null){
       $scope.addFavorite(post);
     }
     else {
-      console.log("I'm in the delete conditional");
-      console.log(post.post_favorite);
-      post.post_favorite.$delete();
+      //instantiate a new Favorite object with all properties of the object to be deleted
+      x = new Favorite({id: post.post_favorite.id, user_id: post.post_favorite.id, created_at: post.post_favorite.created_at, updated_at: post.post_favorite.updated_at});
+      //delete the Favorite object
+      x.$delete();
+      //instantiate a new empty Favorite object
       post.post_favorite = new Favorite({id: null, user_id: null, post_id: null, created_at: null, updated_at: null});
     }
   }
