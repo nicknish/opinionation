2f88365d9b1b00ff2b1efd993d1944f8b1e0971d
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-20 19:42:16 -0700

    refactor infinite scroll to reload previously viewed posts that are removed from the top of the view when new posts are loaded into the view

--- app/assets/javascripts/angularapp/controllers/post_controller.js
+++ app/assets/javascripts/angularapp/controllers/post_controller.js
@@ -5,30 +5,69 @@ opinionationApp.controller('PostCtrl', ['$scope', 'Post', function($scope, Post)
     $scope.posts = json;
   });
 
-  var counter = 2;
-
-  $scope.loadPage = function() {
-    var newPosts = Post.query({page: counter});
+  //called inside the infiniteScroll function to query API for more posts
+  $scope.loadMore = function(pageNum, topOrBottom) {
+    var newPosts = Post.query({page: pageNum});
     newPosts.$promise.then(function(fulfilledPromise){
-      console.log(fulfilledPromise);
       for(i = 0; i < fulfilledPromise.length; i++){
-        $scope.posts.push(fulfilledPromise[i]);
+        if(topOrBottom == "bottom"){
+          $scope.posts.push(fulfilledPromise[i]);
+        }
+        else if(topOrBottom == "top"){
+          $scope.posts.splice(i, 0, fulfilledPromise[i]);
+        }
       }
     });
-    counter += 1;
   }
 
-  //move infinite scroll into a custom directive
-  var ready = true;
+  //initialize vars for infinite scroll function
+  var readyBottom = true;
+  var readyTop = true;
+  var pageCounter = 2;
+  var postsPerQuery = 12;
 
-  $(window).scroll(function(){
-    if ($(window).scrollTop() > $(document).height() - $(window).height() - 50 && ready){
-      ready = false;
-      $scope.loadPage();
-      setTimeout(function(){ready = true}, 3000);
+  //move infinite scroll into a custom directive?
+  var infiniteScroll = $(window).scroll(function(){
+    //only run function if posts have been loaded
+    if($scope.posts.length != undefined){
+      //if scroll hits the bottom, fetch new posts
+      if($(window).scrollTop() > $(document).height() - $(window).height() - 50 && readyBottom){
+        readyBottom = false;
+        $scope.loadMore(pageCounter, "bottom");
+        pageCounter++;
+        if($scope.posts.length >= 48){
+          //keeps $scope.posts array to a maximum of 48 elements
+          $scope.posts.splice(0, postsPerQuery);
+        }
+        //throttle to prevent function from being called in quick succession
+        setTimeout(function(){readyBottom = true}, 2000);
+      }
+      //returns posts previously viewed that were removed from DOM if scroll hits top and all other conditions are met
+      else if($scope.posts.length >= 48 && $(window).scrollTop() == 0 && readyTop && pageCounter !=3){
+        readyTop = false;
+        $scope.loadMore(pageCounter - 3, "top");
+        
+        //returns scroll position to the most recently viewed post
+        (function(){
+          if($(window).width() > 639){
+            $('html,body').animate({scrollTop: $("#post_0").height() * 6}, 600);
+          }
+          else {
+           $('html,body').animate({scrollTop: $("#post_0").height() * 12}, 600); 
+          }
+        })();
+
+        pageCounter--;
+        
+        //keeps $scope.posts array to a maximum of 48 elements
+        $scope.posts.splice($scope.posts.length - postsPerQuery, postsPerQuery);
+        
+        //throttle to prevent function from being called in quick succession
+        setTimeout(function(){readyTop = true}, 2000);
+      }
     }
   });
-  
+
   // Flip checks if a post is flipped,
   // which an ng-class will respond by adding or 
   // removing the 'flipped' class
