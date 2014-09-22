24e73e7334ed7383524359309f420e740a1f5c9a
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-22 15:01:09 -0700

    add validation to answer_controller to prevent an empty answer from showing up in the list of answers

--- app/assets/javascripts/angularapp/controllers/answer_controller.js
+++ app/assets/javascripts/angularapp/controllers/answer_controller.js
@@ -7,13 +7,15 @@ opinionationApp.controller('AnswerCtrl', ['$scope', 'Answer', function($scope, A
   
 
   this.addAnswer = function(post_id){
-    $scope.newAnswer = new Answer();
-    $scope.newAnswer.body = $scope.body;
-    $scope.newAnswer.post_id = post_id;
-    $scope.newAnswer.$save();
-    console.log($scope.newAnswer);
-    $scope.answers.unshift($scope.newAnswer);
-    $scope.body = "";
+    if($scope.body != undefined && $scope.body != ""){
+      $scope.newAnswer = new Answer();
+      $scope.newAnswer.body = $scope.body;
+      $scope.newAnswer.post_id = post_id;
+      $scope.newAnswer.$save();
+      console.log($scope.newAnswer);
+      $scope.answers.unshift($scope.newAnswer);
+      $scope.body = "";
+    }
   }
 
 }]);
