89b3109e08983cd8a9099c0e0d2590b736d03db8
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-23 15:31:03 -0700

    add content to user profile view -- needs styling

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,10 +1,14 @@
 class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :answerer, :body, :vote_count, :post_id, :user_vote, :created_at
+  attributes :id, :answerer, :body, :vote_count, :post_id, :user_vote, :created_at, :question
 
   delegate :current_user, to: :scope
 
   def answerer
     a = {username: object.user.username, user_pic: object.user.profile_pic(:small), id: object.user.id}
+  end
+
+  def question
+    q = {question: object.post.question, poster: object.post.user.username, post_pic: object.post.post_pic(:medium)}
   end
 
   def vote_count
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :question, :poster, :post_pic, :post_favorite
+  attributes :id, :question, :poster, :post_pic, :post_favorite, :num_favs, :created_at
 
   delegate :current_user, to: :scope
 
@@ -9,6 +9,10 @@ class PostSerializer < ActiveModel::Serializer
 
   def post_pic
     object.post_pic ? object.post_pic(:large) : object.user.profile_pic(:large)
+  end
+
+  def num_favs
+    object.favorites.count
   end
 
   #refactor to move to AnswerSerializer?
