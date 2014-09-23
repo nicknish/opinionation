f7913d44a8f0d5df70313db18dee2f558e36a916
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-09-22 18:30:34 -0700

    add index to username column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920232543) do
+ActiveRecord::Schema.define(version: 20140923010144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 20140920232543) do
     t.boolean  "verified_user",            default: false
     t.boolean  "deactivated_user",         default: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
   create_table "votes", force: true do |t|
     t.integer  "user_id"
